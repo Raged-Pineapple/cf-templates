0b2938ab15c9d9c3d0dac7562ab1857839e5d29e
ANIRUDH C <153420003+Raged-Pineapple@users.noreply.github.com>
2024-12-20 23:46:37 +0530

    Create stdtemplate.cpp

---
+++ Implementation/stdtemplate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+
+const int MOD=1e9+7;
+const int INF=LLONG_MAX/2;
+
+signed main() {
+   ios::sync_with_stdio(false);cin.tie(NULL);
+   int t;
+   cin>>t;
+
+
+}
