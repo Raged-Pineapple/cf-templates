8d4988cc2b623387cb9508cab2a49ed50f39fc7a
ANIRUDH C <153420003+Raged-Pineapple@users.noreply.github.com>
2025-04-21 12:25:24 +0530

    Create shortest-distance-using-BFS.cpp

---
+++ shortest-distance-using-BFS.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Back-end complete function Template for C++
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the shortest path from source to all other nodes
+    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+        int n = adj.size();
+        vector<int>dist(n);
+        for(int i=0;i<n;i++){
+            dist[i] = 1e9;
+        }
+        dist[src]=0;
+        queue<int>q;
+        q.push(src);
+        vector<int>ans(n,-1);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it : adj[node]){
+                if(dist[node]+1 < dist[it]){
+                    dist[it] = dist[node]+1;
+                    q.push(it);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+                if(dist[i] != 1e9)ans[i] = dist[i];
+            }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+
+        vector<int> res = obj.shortestPath(adj, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+        cout << "~\n";
+    }
+}
+
+// } Driver Code Ends
